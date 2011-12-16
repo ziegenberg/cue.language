d2e8ee6a8edba752a7d190c0a425b67d9ccea79e
Daniel Ziegenberg <daniel@ziegenberg.at>
2011-12-16 15:45:04 +0100

    added Support for twitter names

--- src/cue/lang/WordIterator.java
+++ src/cue/lang/WordIterator.java
@@ -25,29 +25,33 @@ import java.util.regex.Pattern;
  * 
  */
 public class WordIterator extends IterableText {
-            + "+)*");
-    private final Matcher m;
-    private boolean hasNext;
+	private static final String LETTER = "[@#+\\p{javaLetterOrDigit}]";
+	private static final String JOINER = "[-_.:/'’\\p{M}\\u2032\\u00A0\\u200C\\u200D~]";
+	private static final Pattern WORD = Pattern.compile(LETTER + "+(" + JOINER
+			+ "+" + LETTER + "+)*");
 
-    public WordIterator(final String text) {
-        this.m = WORD.matcher(text == null ? "" : text);
-        hasNext = m.find();
-    }
+	private final Matcher m;
+	private boolean hasNext;
 
-    public void remove() {
-        throw new UnsupportedOperationException();
-    }
+	public WordIterator(final String text) {
+		this.m = WORD.matcher(text == null ? "" : text);
+		hasNext = m.find();
+	}
 
-    public String next() {
-        if (!hasNext) {
-            throw new NoSuchElementException();
-        }
-        final String s = m.group();
-        hasNext = m.find();
-        return s;
-    }
+	public void remove() {
+		throw new UnsupportedOperationException();
+	}
 
-    public boolean hasNext() {
-        return hasNext;
-    }
+	public String next() {
+		if (!hasNext) {
+			throw new NoSuchElementException();
+		}
+		final String s = m.group();
+		hasNext = m.find();
+		return s;
+	}
+
+	public boolean hasNext() {
+		return hasNext;
+	}
 }
